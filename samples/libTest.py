c7b25fd53e64cbd3b87a494deee72f5d389745a2
Yoshihiko Aochi <yoshihiko.aochi@nuance.com>
2016-03-25 14:54:38 +0900

    Changes to be committed: 	new file:   samples/libTest.py

---
+++ samples/libTest.py
@@ -0,0 +1,42 @@
+from opsclient import OPSClient
+
+ops_server = '<put your server address>'
+client = OPSClient(ops_server)
+
+print client.server
+
+client.getJobTypes()
+jobId = client.createJob(18, 'test', 'test', 'test')
+storeUrls = client.getUploadUrls(jobId, 1)
+client.postInputFile('sample.png', storeUrls[0])
+request_values = ( {
+                'CompletionNotifications' : [{
+                        'AuthenticationHeader': None,
+                        'CertificateSubject' : None,
+                        'Parameters': None,
+                        'ServiceUrl' : 'http://<put your client address>:8000/Success'
+                }],
+                'FailureNotifications' : [{
+                        'AuthenticationHeader': None,
+                        'CertificateSubject' : None,
+                        'Parameters': None,
+                        'ServiceUrl' : 'http://<put your client address>:8000/Failed'
+                }]
+                }
+            )
+client.configureJobNotification(jobId, request_values)
+conversionParams = ('<ConversionParameters xmlns="http://www.nuance.com/2011/ConversionParameters">'
+'<ImageQuality>Better</ImageQuality>'
+'<LogicalFormRecognition>No</LogicalFormRecognition>'
+'<Language>LANG_JPN</Language>'
+'<Rotation>Auto</Rotation>'
+'<Deskew>No</Deskew>'
+'<TradeOff>Balanced</TradeOff>'
+'<LayoutTradeOff>Accuracy</LayoutTradeOff>'
+'<PDFCompatibility>PDF1.6</PDFCompatibility>'
+'<CacheInputForReuse>True</CacheInputForReuse>'
+'</ConversionParameters>')
+client.startJob(jobId, conversionParams)
+client.startLocalNotificationServer(8000, 3)
+downloadUrls = client.getDownloadUrls(jobId)
+client.downloadFile(downloadUrls[0], 'test0000.pdf')
