ef3ef7185989800332005725f791227bec5b9ad3
Yoshihiko Aochi <yoshihiko.aochi@nuance.com>
2016-03-27 11:30:42 +0900

    Changes to be committed: 	modified:   opsclient/__init__.py 	modified:   setup.py

--- opsclient/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bun//env python
-# config: utf-8
-from opsclient import OPSClient
-__author__  = 'Yoshihiko Aochi'
-__version__ = '0.0.1'
-__license__ = 'MIT'
--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
 from setuptools import setup, find_packages
-from opsclient import __author__, __version__, __license__
+
 
 setup(
 	name = 'opsclient',
-	version = __version__,
+	version = '0.0.1',
 	description = 'Python cliebt library for Nuance OmniPage Server',
-	license = __license__,
-	author = __author__,
+	license = 'MIT',
+	author = 'Yoshihiko Aochi',
 	author_email = 'yoshihiko.aochi@nuance.com',
 	url = 'https://github.com/',
 	keywords = 'pip github python ops omnipage',
